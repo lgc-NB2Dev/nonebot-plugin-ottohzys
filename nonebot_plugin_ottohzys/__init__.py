eeb5dd3dc8ac68984fa3a8677c709970cc94a15d
student_2333 <lgc2333@126.com>
2025-01-18 21:15:15 +0800

    up

--- nonebot_plugin_ottohzys/__init__.py
+++ nonebot_plugin_ottohzys/__init__.py
@@ -14,5 +14,5 @@ __plugin_meta__ = PluginMetadata(
     homepage="https://github.com/lgc-NB2Dev/nonebot-plugin-ottohzys",
     config=ConfigModel,
     supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
-    extra={"License": "MIT", "Author": "student_2333"},
+    extra={"License": "MIT", "Author": "LgCookie"},
 )
